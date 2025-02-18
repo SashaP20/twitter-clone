c68106ce3229c41037ddf3f7135a568d5b9eca09
Sasha191312 <alex.perednya20@gmail.com>
2025-02-18 21:47:13 +0200

    fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,63 +1,62 @@
-import { Routes,Route, Navigate } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
-import SignupPage from "./pages/auth/signup/SignupPage";
-import Sidebar from "./components/common/Sidebar";
-import RightPanel from "./components/common/RightPanel";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
 import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
+
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+
 import { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
 
-
 function App() {
-  const {data: authUser,isLoading,error,isError} = useQuery({
-    // we use queryKey to give a unique name to our query
-    queryKey: ['authUser'],
-    queryFn: async () => {
-      try{
-        const res = await fetch('/api/auth/me');
-        const data = await res.json();
-        if(data.error) return null;
-        if(!res.ok){
-          throw new Error(data.error || 'Something went wrong');
-        }
-        console.log("authUser is here: ",data);
-        return data;
+	const { data: authUser, isLoading } = useQuery({
+		// we use queryKey to give a unique name to our query and refer to it later
+		queryKey: ["authUser"],
+		queryFn: async () => {
+			try {
+				const res = await fetch("/api/auth/me");
+				const data = await res.json();
+				if (data.error) return null;
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+				console.log("authUser is here:", data);
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		retry: false,
+	});
 
-      } catch(error){
-        throw new Error(error);
+	if (isLoading) {
+		return (
+			<div className='h-screen flex justify-center items-center'>
+				<LoadingSpinner size='lg' />
+			</div>
+		);
+	}
 
-      }
-    },
-    retry:false, // retry once
-  });
-
-  if(isLoading){
-    return (
-      <div className="h-screen flex justify-center items-center">
-        <LoadingSpinner size="lg"/>
-      </div>
-    )
-  }
-
-  return (
-    <div className="flex max-w-6xl">
-     {authUser &&  <Sidebar />}
-      <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-        <Route path="/signup" element={!authUser ? <SignupPage /> : <Navigate to="/" />} />
-        <Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
-        <Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
-
-
-      </Routes>
-      <RightPanel/>
-      <Toaster />
-    </div>
-  );
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			{/* Common component, bc it's not wrapped with Routes */}
+			{authUser && <Sidebar />}
+			<Routes>
+				<Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
+				<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
+				<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
+				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
+			</Routes>
+			{authUser && <RightPanel />}
+			<Toaster />
+		</div>
+	);
 }
 
-export default App
+export default App;
--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -5,44 +5,46 @@ import XSvg from "../../../components/svgs/X";
 
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
-import { useMutation, useQuery, useQueryClient} from '@tanstack/react-query';
-import toast from "react-hot-toast";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const LoginPage = () => {
 	const [formData, setFormData] = useState({
 		username: "",
 		password: "",
 	});
-
 	const queryClient = useQueryClient();
 
-	const {mutate: loginMutation, isError,isPending,error} = useMutation({
-		mutationFn: async ({username,password}) => {
-			try{
-				const res = await fetch('/api/auth/login', {
-					method: 'POST',
-					headers:{
-						"Content-Type": "application/json"
+	const {
+		mutate: loginMutation,
+		isPending,
+		isError,
+		error,
+	} = useMutation({
+		mutationFn: async ({ username, password }) => {
+			try {
+				const res = await fetch("/api/auth/login", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
 					},
-					body: JSON.stringify({username,password})
+					body: JSON.stringify({ username, password }),
 				});
-				const data = await res.json();
-				if(!res.ok) throw new Error(data.error || "Failed to login");
-				console.log(data);
-				// return data;
-			} catch(error){
-				console.error(error);
-				throw error;
 
+				const data = await res.json();
+
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+			} catch (error) {
+				throw new Error(error);
 			}
 		},
-		onSuccess: (data) => {
-			// refetch the authUser query
-			queryClient.invalidateQueries({queryKey: ['authUser']});
+		onSuccess: () => {
+			// refetch the authUser
+			queryClient.invalidateQueries({ queryKey: ["authUser"] });
 		},
-		
 	});
-	
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -52,7 +54,6 @@ const LoginPage = () => {
 	const handleInputChange = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
-
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen'>
@@ -86,7 +87,9 @@ const LoginPage = () => {
 							value={formData.password}
 						/>
 					</label>
-					<button className='btn rounded-full btn-primary text-white'>{isPending ? "Loading..." : "Login"}</button>
+					<button className='btn rounded-full btn-primary text-white'>
+						{isPending ? "Loading..." : "Login"}
+					</button>
 					{isError && <p className='text-red-500'>{error.message}</p>}
 				</form>
 				<div className='flex flex-col gap-2 mt-4'>
